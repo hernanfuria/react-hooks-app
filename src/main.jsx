00471e222d6f293fc987b4de67461b3c695d874a
hernanfuria <hernan.furiasse@hotmail.com>
2023-06-12 18:11:50 -0300

    useRef

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,11 @@ import './index.css'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook.jsx'
 // import { SimpleForm } from './02-useEffect/SimpleForm.jsx'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook.jsx'
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+import { FocusScreen } from './04-useRef/FocusScreen'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-    <MultipleCustomHooks />
+    <FocusScreen />
   //</React.StrictMode>,
 )
