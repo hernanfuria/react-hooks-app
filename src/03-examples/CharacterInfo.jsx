385eafec73de3d59348b3a8b5e74270ebfc33cc1
hernanfuria <hernan.furiasse@hotmail.com>
2023-06-12 18:54:36 -0300

    useLayoutEffect

--- src/03-examples/CharacterInfo.jsx
+++ src/03-examples/CharacterInfo.jsx
@@ -1,9 +1,29 @@
-export const CharacterInfo = ({name, image, gender, status}) => {
-  return (
-    <>
-        <h1>{name}</h1>
-        <img src={image} />
-        <p>{gender} | {status}</p>
-    </>
-  )
+import { useLayoutEffect, useRef, useState } from "react";
+
+export const CharacterInfo = ({name, image, gender, status, episode}) => {
+    const pRef = useRef();
+    const [boxSize, setBoxSize] = useState({width: 0, height: 0})
+    
+    useLayoutEffect(() => {
+        const {width, height} = pRef.current.getBoundingClientRect();
+        setBoxSize({width, height})
+    }, [])
+    
+    const episodesAsText = () => {
+        let t = '';
+        for (const ep of episode) {
+            t += ` ${ep}`;
+        }
+        return t;
+    }
+    
+    return (
+        <div ref={pRef} className="char-info" style={{display: 'grid'}}>
+            <h1>{name}</h1>
+            <img src={image} />
+            <p>{gender} | {status}</p>
+            {episodesAsText()}
+            <code>{JSON.stringify(boxSize)}</code>
+        </div>
+    )
 }
---
+++ src/05-useLayoutEffect/Layout.jsx
@@ -0,0 +1,28 @@
+import { useCounter, useFetch } from "../hooks";
+import { CharacterInfo, LoadingCharacter } from "../03-examples";
+
+export const Layout = () => {
+    const {counter, decrement, reset, increment} = useCounter(1);
+    const {data, isLoading, hasError} = useFetch(`https://rickandmortyapi.com/api/character/${counter}`);
+    
+    console.log(data, isLoading, hasError);
+
+    return (
+        <>
+            <h1>Rick and Morty</h1>
+            <hr />
+
+            {
+                isLoading
+                ? <LoadingCharacter />
+                : <CharacterInfo name={data.name} image={data.image} gender={data.gender} status={data.status} episode={data.episode} />
+
+            }
+
+            <hr />
+            <button className="btn btn-primary" disabled={isLoading} onClick={() => decrement()}>-1</button>
+            <button className="btn btn-primary" disabled={isLoading} onClick={reset}>Reset</button>
+            <button className="btn btn-primary" disabled={isLoading} onClick={() => increment()}>+1</button>
+        </>
+  )
+}
