96254bd4f73ff59746286218cd581dc0b5b78c65
hernanfuria <hernan.furiasse@hotmail.com>
2023-06-15 18:50:09 -0300

    remove_todo agregado al todoReducer

--- src/08-useReducer/todoReducer.js
+++ src/08-useReducer/todoReducer.js
@@ -3,13 +3,14 @@ export const todoReducer = (initialState, action) => {
         case 'add_todo':
             return [...initialState, action.payload];
         case 'remove_todo':
-            const newTodos = [];
-            for (const todo of initialState) {
-                if (todo.id != action.payload) {
-                    newTodos.push(todo);
-                };
-            };
-            return newTodos;
+            // const newTodos = [];
+            // for (const todo of initialState) {
+            //     if (todo.id != action.payload) {
+            //         newTodos.push(todo);
+            //     };
+            // };
+            // return newTodos;
+            return initialState.filter(todo => todo.id !== action.payload);
         default:
             return initialState;
     }
